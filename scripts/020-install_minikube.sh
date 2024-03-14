4a93492b2f68c341bf0402bd8754bd85560b9791
Yuya.Nishida <yuya@j96.org>
2024-03-14 10:57:04 +0900

    git dust commit [ci skip]

--- scripts/020-install_minikube.sh
+++ scripts/020-install_minikube.sh
@@ -11,15 +11,19 @@ sudo dpkg -i minikube_latest_amd64.deb
 rm minikube_latest_amd64.deb
 
 minikube start
-minikube kubectl -- get po -A
-minikube kubectl get nodes
 #minikube dashboard
 
 # https://minikube.sigs.k8s.io/docs/handbook/kubectl/
 sudo ln -sfv $(which minikube) /usr/local/bin/kubectl
 
+# show current status
+kubectl get po -A
+kubectl get nodes
+
 # https://github.com/helm/helm/releases
+work_path=$(mktemp -d)
+cd $work_path
 wget https://get.helm.sh/helm-v3.14.3-linux-amd64.tar.gz
 tar xf helm-v3.14.3-linux-amd64.tar.gz
 sudo install -v -m 755 linux-amd64/helm /usr/local/bin/
-rm -rf helm-v3.14.3-linux-amd64.tar.gz linux-amd64
+rm -rf $work_path
