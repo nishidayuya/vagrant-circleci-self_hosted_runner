34d82e4fc07bcd8308b5588c5144a5cf78fc8aa5
Yuya.Nishida <yuya@j96.org>
2024-03-18 10:25:44 +0900

    git dust commit [ci skip]

--- scripts/020-install_minikube.sh
+++ scripts/020-install_minikube.sh
@@ -21,13 +21,3 @@ sudo ln -sfv $(which minikube) /usr/local/bin/kubectl
 # show current status
 kubectl get po -A
 kubectl get nodes
-
-# TODO: APT化https://helm.sh/ja/docs/intro/install/#apt-%E3%81%8B%E3%82%89-debianubuntu
-# https://github.com/helm/helm/releases
-work_path=$(mktemp -d)
-cd $work_path
-wget https://get.helm.sh/helm-v3.14.3-linux-amd64.tar.gz
-tar xf helm-v3.14.3-linux-amd64.tar.gz
-sudo install -v -m 755 linux-amd64/helm /usr/local/bin/
-rm -rf $work_path
-cd
---
+++ scripts/030-install_helm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+# TODO: APT化https://helm.sh/ja/docs/intro/install/#apt-%E3%81%8B%E3%82%89-debianubuntu
+# https://github.com/helm/helm/releases
+work_path=$(mktemp -d)
+cd $work_path
+wget https://get.helm.sh/helm-v3.14.3-linux-amd64.tar.gz
+tar xf helm-v3.14.3-linux-amd64.tar.gz
+sudo install -v -m 755 linux-amd64/helm /usr/local/bin/
+rm -rf $work_path
+cd
