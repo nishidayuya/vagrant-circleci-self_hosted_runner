f6ae91d7548af551afc5a29066fe1ab0fb7a3efb
Yuya.Nishida <yuya@j96.org>
2024-03-18 12:34:22 +0900

    git dust commit [ci skip]

--- scripts/030-install_helm.sh
+++ scripts/030-install_helm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 # TODO: APT化https://helm.sh/ja/docs/intro/install/#apt-%E3%81%8B%E3%82%89-debianubuntu
 # https://github.com/helm/helm/releases
@@ -9,5 +9,5 @@ cd $work_path
 wget https://get.helm.sh/helm-v3.14.3-linux-amd64.tar.gz
 tar xf helm-v3.14.3-linux-amd64.tar.gz
 sudo install -v -m 755 linux-amd64/helm /usr/local/bin/
-rm -rf $work_path
 cd
+rm -rf $work_path
